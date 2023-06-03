5c197931177d490b495435cf5786d149388b5881
oxford <olo0704@wp.pl>
2023-06-03 12:58:47 +0200

    Initial commit

---
+++ buildAndRun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd out/build
+make -j4
+
+#cd app
+#./unitTests
+#
+#cd ../test
+#./testsRunner
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by oxford on 30.05.23.
+//
+#include "stdio.h"
+#include "stm32l476xx.h"
+
+int main()
+{
+    return 0;
+}
